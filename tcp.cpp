7001fbe85b856db025b970ae67e0a2d0e49d550b
KolYev <evgeniy.kolesenko@gmail.com>
2026-02-07 17:44:31 +0300

    TCPSocketHandler::run()

--- tcp.cpp
+++ tcp.cpp
@@ -61,3 +61,80 @@ bool TCPSocketHandler::startClient() {
     return true;
     
 }
+
+void TCPSocketHandler::chatLoop() {
+    socket.setBlocking(false);
+
+    std::string message = "";
+    sf::Packet packet;
+
+    std::cout << "\n=== TCP чат начался ===\n";
+    std::cout << "Напишите ваше сообщение и нажмите Enter, чтобы отправить\n";
+    std::cout << "Напишите 'exit' для выхода\n\n";
+
+    while (true)
+    {
+        // Проверка, была ли нажата клавиша
+        if (_kbhit())
+        {
+            // Считываем строку с сообщением
+            std::getline(std::cin, message);
+            
+            // Если сообщение не пустое
+            if (!message.empty())
+            {
+                // Проверка команду выхода
+                if (message == "exit" || message == "quit")
+                {
+                    break;
+                }
+
+                // Очищаем пакет от предыдущих данных
+                packet.clear();
+                // Упаковываем имя и сообщение в пакет
+                packet << username << message;
+
+                // Отправляем пакет через сокет
+                if (socket.send(packet) != sf::Socket::Status::Done)
+                {
+                    std::cout << "Ошибка отправки сообщения!\n";
+                }
+                else
+                {
+                    std::cout << "You: " << message << '\n';
+                }
+
+                // Очищаем переменную сообщения
+                message = "";
+            }
+        }
+
+        // Принимаем данные через сокет
+        sf::Socket::Status status = socket.receive(packet);
+        // Если получено сообщение
+        if (status == sf::Socket::Status::Done)
+        {
+            // Переменные для распаковки данных
+            std::string nameRec;
+            std::string messageRec;
+
+            // Распаковываем имя и сообщение из пакета
+            if (packet >> nameRec >> messageRec)
+            {
+                // Выводим полученное сообщение
+                std::cout << nameRec << ": " << messageRec << '\n';
+            }
+        }
+        // Если соединение разорвано
+        else if (status == sf::Socket::Status::Disconnected)
+        {
+            std::cout << "Соединение прервано!\n";
+            break;
+        }
+
+        // Небольшая задержка
+        std::this_thread::sleep_for(std::chrono::milliseconds(10));
+    }
+
+    std::cout << "TCP чат закончился.\n";
+}
