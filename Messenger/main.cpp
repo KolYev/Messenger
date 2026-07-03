1beef0ca2d3845e0b0826064a5960f54ae7b0a5f
KolYev <evgeniy,kolesenko@gmail.com>
2026-07-03 23:00:43 +0300

    функция получения публичного IP адреса

--- Messenger/main.cpp
+++ Messenger/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include <string>
 #include <WinSock2.h>
+#include <winhttp.h>
 #pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "winhttp.lib")
 
 using namespace std;
 
@@ -54,16 +57,45 @@ public:
 	}
 };
 
+// получение публичного IP адреса
+string get_public_ip()
+{
+    string ip;
+    HINTERNET session = WinHttpOpen(L"Messenger/1.0", WINHTTP_ACCESS_TYPE_DEFAULT_PROXY, NULL, NULL, 0);
+    if (!session) return "error";
+
+    HINTERNET connect = WinHttpConnect(session, L"api.ipify.org", INTERNET_DEFAULT_HTTP_PORT, 0);
+    if (!connect) { WinHttpCloseHandle(session); return "error"; }
+
+    HINTERNET request = WinHttpOpenRequest(connect, L"GET", NULL, NULL, NULL, NULL, 0);
+    if (!request) { WinHttpCloseHandle(connect); WinHttpCloseHandle(session); return "error"; }
+
+    WinHttpSendRequest(request, NULL, 0, NULL, 0, 0, 0);
+    WinHttpReceiveResponse(request, NULL);
+
+    char buf[64];
+    DWORD bytes = 0;
+    while (WinHttpReadData(request, buf, sizeof(buf) - 1, &bytes) && bytes > 0) {
+        buf[bytes] = '\0';
+        ip += buf;
+    }
+
+    WinHttpCloseHandle(request);
+    WinHttpCloseHandle(connect);
+    WinHttpCloseHandle(session);
+    return ip;
+}
 
 int main()
 {
 	SetConsoleCP(65001);
 	SetConsoleOutputCP(65001);
-	
+
+	cout << "Публичный IP: " << get_public_ip() << endl;
+
 	Server server(4000);
 	server.bind_and_listen();
 	server.accept_client();
-	
 
 	cout << "Сообщение от клиента: " << server.receive_message() << endl;
 
